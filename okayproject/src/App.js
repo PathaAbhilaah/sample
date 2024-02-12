bb4313533618cf1add5aa8ee6ca7fad2b0cde5e6
Abhilash <abhilashpatha0111@gmail.com>
2024-02-12 08:04:16 +0530

    made change in newTask branch

--- okayproject/src/App.js
+++ okayproject/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     This is the main branch
+     This is the main branch and the updated code of newTask branch
     </div>
   );
 }
